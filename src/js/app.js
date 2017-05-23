d243d2f7c0ff5bfad59fcae634a8199c243d9c11
Reginaldo Santos <rmsantos@br.ibm.com>
2017-05-22 23:07:39 -0300

    making Channels dynamic from web api

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,57 @@
-var socket = io('//localhost:3000')
+$(document).ready(function() {
+  (function() {
+    var getRooms = function() {
+            return $.get('http://localhost:3000/rooms', function(data) {
+                if (!data.status) {
+                    return
+                }
 
-$('#message').keypress(function(e) {
-  if (e.which == 13) {
-    var val = $('#message').val()
+                var rooms = data && data.rooms
+                console.log(rooms)
 
-    socket.emit('message', {
-      message: val
-    })
+        var titleTemplate = '<li class="list-group-item title"> ' +
+        					          '  <h4>Channels (' + rooms.length +')</h4> ' +
+        				            '</li>'
 
-    return false
-  }
-})
+        $('.channels').append(titleTemplate)
 
-socket.on('message', function(data) {
-  var template =  '<div class="col-xs-12 message">' +
-                  '  <div class="avatar col-xs-6 col-md-1">' +
-                  '     <h2>{`.`}</h2>' +
-                  '  </div>' +
-                  '  <p class="text col-xs-6 col-md-11">' + data.message + '</p>' +
-                  '</div>'
+        rooms.forEach(function(room, index) {
+          var roomTemplate = '<li class="list-group-item" channel="' + room._id + '">'+
+                              ' <i class="fa fa-comment-o"></i> '+
+                               room.name +
+                              '</li>'
+          $('.channels').append(roomTemplate)
+        })
 
-                  $('.conversation').append(template)
+
+      })
+    }
+    getRooms()
+  })()
+
+  var socket = io('//localhost:3000')
+
+  $('#message').keypress(function(e) {
+    if (e.which == 13) {
+      var val = $('#message').val()
+
+      socket.emit('message', {
+        message: val
+      })
+
+      return false
+    }
+  })
+
+  socket.on('message', function(data) {
+    var template =  '<div class="col-xs-12 message">' +
+                    '  <div class="avatar col-xs-6 col-md-1">' +
+                    '     <h2>{`.`}</h2>' +
+                    '  </div>' +
+                    '  <p class="text col-xs-6 col-md-11">' + data.message + '</p>' +
+                    '</div>'
+
+                    $('.conversation').append(template)
+  })
+
 })
